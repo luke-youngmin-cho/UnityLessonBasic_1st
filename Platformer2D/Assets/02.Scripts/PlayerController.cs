ffde1d04b8c3b7fd0a6572dc7837542782dac42f
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-02-23 21:58:21 +0900

    Platformer2D
    1. using FSM model in another FSM
    2. how to use Rigidbody.velocity
    3. 2 way to change player's direction (euler angle vs flip renderer)

--- Platformer2D/Assets/02.Scripts/PlayerController.cs
+++ Platformer2D/Assets/02.Scripts/PlayerController.cs
@@ -10,10 +10,29 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
-    PlayerState playerState;
+    int _direction;
+    int direction
+    {
+        set
+        {
+            _direction = value;
+            if (_direction < 0)
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            else if (_direction > 0)
+                transform.eulerAngles = Vector3.zero;
+        }
+        get { return _direction; }
+    }
+
+    // States    
+    public PlayerState playerState;
+    public JumpState jumpState;
 
     // Detectors
     PlayerGroundDetector groundDetector;
+
+    // animation
+    float animationTimeElapsed;
     private void Awake()
     {
         tr = GetComponent<Transform>();
@@ -26,12 +45,25 @@ public class PlayerController : MonoBehaviour
     {
         // 키보드 좌우 입력 받아서 게임오브젝트를 좌우로 움직이는 기능
         float h = Input.GetAxis("Horizontal");
-        rb.position += new Vector2(h * moveSpeed * Time.deltaTime, 0);
+        if (h < 0)
+            direction = -1;
+        else if (h > 0)
+            direction = 1;
 
+
+        rb.position += new Vector2(h * moveSpeed * Time.deltaTime, 0);
+        //rb.velocity = new Vector2(h * moveSpeed , 0); 
+        // Rigidbody.velocity 를 물리연산 주기마다 실행할 경우 
+        // 비정상적인 동작을 일으킬 가능성이 있으므로 
+        // 주기함수에서는 velocity 가 아니라 position 을 변경하는 방식으로 움직인다.
+        // velocity 를 직접 수정하는 경우는 
+        // 점프하는 순간 등 의 경우에 순간적으로 속도가 바뀌어야 할때 
+        // 또는 특정 동작에서 다른 동작으로 넘어가는 순간 속도를 재설정 해야할때 직접수정.
+        
         if (playerState != PlayerState.Jump && Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             playerState = PlayerState.Jump;
+            jumpState = JumpState.PrepareToJump;
         }
         UpdatePlayerState();
     }
@@ -45,18 +77,48 @@ public class PlayerController : MonoBehaviour
             case PlayerState.Run:
                 break;
             case PlayerState.Jump:
-                if (groundDetector.isGrounded)
-                    playerState = PlayerState.Idle;
+                UpdateJumpState();
                 break;
             default:
                 break;
         }
     }
-
-    enum PlayerState
+    void UpdateJumpState()
+    {
+        switch (jumpState)
+        {
+            case JumpState.PrepareToJump:
+                //todo -> changeAnimation
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0f, jumpForce),ForceMode2D.Impulse);
+                jumpState = JumpState.Jumping;
+                break;
+            case JumpState.Jumping:
+                if(groundDetector.isGrounded == false)
+                {
+                    jumpState = JumpState.InFlight;
+                }
+                break;
+            case JumpState.InFlight:
+                if (groundDetector.isGrounded)
+                {
+                    playerState = PlayerState.Idle;
+                    jumpState = JumpState.Idle;
+                }
+                break;
+        }
+    }
+    public enum PlayerState
     {
         Idle,
         Run,
         Jump,
+    }
+    public enum JumpState
+    {
+        Idle,
+        PrepareToJump, // Jump 에 필요한 파라미터 세팅, 애니메이션 전환 등 
+        Jumping, // Jump 물리연산을 시작하는 단계 
+        InFlight, // Jump 물리연산이 끝나고 공중에 캐릭터가 떠있는 상태
     }
 }
--- Platformer2D/Assets/02.Scripts/PlayerGroundDetector.cs
+++ Platformer2D/Assets/02.Scripts/PlayerGroundDetector.cs
@@ -9,6 +9,7 @@ public class PlayerGroundDetector : MonoBehaviour
     Vector2 center;
     public Vector2 size;
 
+    public LayerMask groudLayer;
     // 다른 클래스에서는 접근을 해야하지만 인스펙터창에서는 숨기고싶을때
     [HideInInspector] public bool isGrounded;
     private void Awake()
@@ -21,8 +22,8 @@ public class PlayerGroundDetector : MonoBehaviour
     void Update()
     {
         center.x = rb.position.x + col.offset.x;
-        center.y = rb.position.y + col.offset.y - col.size.y / 2 - size.y;
-        isGrounded = Physics2D.OverlapBox(center, size, 0);
+        center.y = rb.position.y + col.offset.y - col.size.y / 2 - size.y * 2;
+        isGrounded = Physics2D.OverlapBox(center, size, 0, groudLayer);
     }
     private void OnDrawGizmosSelected()
     {
