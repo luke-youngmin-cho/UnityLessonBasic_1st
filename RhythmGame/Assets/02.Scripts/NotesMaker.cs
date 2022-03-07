8506e2a4e256f5db68e5ba7195e5e76116caccc5
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-03-07 22:05:54 +0900

    RhythmGame
    1. FlowChart & Class Concept
    2. Added SongData , NoteData, NotesMaker classes

---
+++ RhythmGame/Assets/02.Scripts/NotesMaker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Video;
+public class NotesMaker : MonoBehaviour
+{
+    SongData songData;
+    KeyCode[] keyCodes = { KeyCode.S, KeyCode.D, KeyCode.F, KeyCode.Space, KeyCode.J, KeyCode.K, KeyCode.L };
+
+    public VideoPlayer vp;
+    public bool onRecord
+    {
+        set
+        {
+            if (value)
+                StartRecording();
+            else
+                StopRecording();
+        }
+        get { return vp.isPlaying; }
+    }
+    private void Update()
+    {
+        if (onRecord)
+            Recording();
+    }
+    private void StartRecording()
+    {
+        songData = new SongData();
+        songData.videoName = vp.clip.name;
+        vp.Play();
+    }
+    private void Recording()
+    {
+        foreach (KeyCode keyCode in keyCodes)
+        {
+            if(Input.GetKeyDown(keyCode))
+                CreateNoteData(keyCode);
+        }
+        if (Input.GetKeyDown(KeyCode.Insert))
+            SaveSongData();
+    }
+    private void StopRecording()
+    {
+        songData = null;
+        vp.Stop();
+    }
+    private void CreateNoteData(KeyCode keyCode)
+    {
+        NoteData noteData = new NoteData();
+        float roundedTime = (float)Math.Round(vp.time, 2);
+        noteData.time = roundedTime;
+        noteData.keyCode = keyCode;
+        songData.notes.Add(noteData);
+    }
+    private void SaveSongData()
+    {
+        // panel 만 띄우고 선택시 디렉토리 문자열 반환 (저장하지 않음)
+        string dir = EditorUtility.SaveFilePanel("저장할 곳을 지정하세요", "", $"{songData.videoName}", "json");
+        // 실제 song data 를 json 포멧으로 저장
+        System.IO.File.WriteAllText(dir, JsonUtility.ToJson(songData));
+    }
+}
---
+++ RhythmGame/Assets/02.Scripts/SongData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class SongData
+{
+    public string videoName;
+    public List<NoteData> notes;
+
+    public SongData()
+    {
+        notes = new List<NoteData>();
+    }
+}
